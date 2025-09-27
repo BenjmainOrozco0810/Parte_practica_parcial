9aa8e7bd843c187535e9bc26e7366a9805bd3ea9
Jonathan Benjamin Orozco Coxaj <148999208+BenjmainOrozco0810@users.noreply.github.com>
2025-09-26 19:59:06 -0600

    Parte practica

--- canciones/canciones.js
+++ canciones/canciones.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const PORT = 3001;
-// Endpoint simple de usuarios
+// Endpoint simple de canciones
 app.get("/canciones", (req, res) => {
 res.json([
   {
